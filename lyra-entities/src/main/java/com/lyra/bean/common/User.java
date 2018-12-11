b22ef0dc033ec607f64638c16fe914bdb9be79ca
baryshnikov-oi-180801 <baryshnikov@fsvps.ru>
2018-12-11 22:00:26 +0300

    Добавить права для пользователя.

--- lyra-entities/src/main/java/com/lyra/bean/common/User.java
+++ lyra-entities/src/main/java/com/lyra/bean/common/User.java
@@ -3,7 +3,14 @@ package com.lyra.bean.common;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.lang.Nullable;
+
 import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Entity
 @Table(name = "Users")
@@ -12,30 +19,95 @@ import javax.persistence.*;
 @EqualsAndHashCode(of = {"login", "email"}, callSuper = true)
 public class User extends GenericLombokEntity{
 
-    private static final long serialVersionUID = -1939385900251608680L;
-
-    @Column(unique = true)
+    @Column(name="login", unique = true)
     private String login;
 
-    @Column
-    private String fio;
+    @Transient
+    public String getFio() {
+        StringJoiner joiner = new StringJoiner(" ");
+        if (lastName != null) {
+            joiner.add(lastName);
+        }
+        if (firstName != null) {
+            joiner.add(firstName);
+        }
+        if (middleName != null) {
+            joiner.add(middleName);
+        }
+        return joiner.toString();
+    }
 
-    @Column
+    @Column(name = "firstName")
     private String firstName;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "middleName")
     private String middleName;
 
-    @Column
+    @Column(name = "email")
     private String email;
 
-    @Column
+    @Column(name = "telephone")
     private String telephone;
 
     @Transient
     private String password;
+
+    @Column(name = "isEnabled")
+    private Boolean enabled;
+
+    @Transient
+    private Set<UserAuthority> authorities = new LinkedHashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "User_UserAuthority",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id"))
+    private Set<UserAuthority> authoritySet;
+
+    @Transient
+    public boolean hasAuthority(@Nullable String authorityName) {
+        if (authorityName == null || authorityName.isEmpty()) {
+            return false;
+        }
+        if (getAuthorities().isEmpty()) {
+            return false;
+        } else {
+            for (UserAuthority userAuthority : getAuthorities()) {
+                if (userAuthority.getName().equals(authorityName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static boolean hasAuthority(@Nullable User user, @Nullable String authorityName) {
+        return !(user == null || (authorityName == null || authorityName.isEmpty())) && user.hasAuthority(authorityName);
+    }
+
+    @Transient
+    public boolean hasAnyAuthorities(Set<String> authorityNameSet) {
+        for (String authorityName : authorityNameSet) {
+            if (hasAuthority(authorityName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasAnyAuthorities(@Nullable User user, Set<String> authorityNameSet) {
+        if (user != null) {
+            for (String authorityName : authorityNameSet) {
+                if (user.hasAuthority(authorityName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
